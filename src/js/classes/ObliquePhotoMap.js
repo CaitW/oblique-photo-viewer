bf462bd40c6eedbf9847358cc87bbef57c8f48e3
CaitW <wolters.caitlin@gmail.com>
2017-02-05 14:06:42 -0500

    Connected layer switcher and map to toggle base maps

--- src/js/classes/ObliquePhotoMap.js
+++ src/js/classes/ObliquePhotoMap.js
@@ -1,12 +1,49 @@
+import CONFIG from '../config.json';
 export default class ObliquePhotoMap {
-	constructor (map) {
-		var self = this;
-		this.map = L.map(map).setView([51.505, -0.09], 13);
-		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(self.map);
-	}
-	toggleLayer(layerName, active) {
-		console.log(layerName, active);
-	}
+    constructor(map) {
+        var self = this;
+        this.baselayerGroup = L.layerGroup();
+        this.layerGroup = L.layerGroup();
+        this.map = L.map(map, {
+                maxBounds: CONFIG.map.maxExtent,
+                layers: [self.baselayerGroup, self.layerGroup]
+            })
+            .fitBounds(CONFIG.map.wisconsinExtent, {
+                padding: [10, 10]
+            });
+        this.baselayerIndex = {};
+        this.layerIndex = {};
+    }
+    toggleLayer(layerID, layer) {
+        var self = this;
+        // temp if
+        if (typeof layer.url === "undefined") {
+        	console.error("Layer " + layerID + " must have a tile URL");
+        }
+        else {
+            if (typeof this.layerIndex[layerID] === "undefined") {
+                this.layerIndex[layerID] = Object.assign({}, layer);
+                this.layerIndex[layerID].layer = L.tileLayer(layer.url);
+            }
+            if (layer.active === true) {
+                let newBaselayer = L.tileLayer(baselayer.url);
+                this.baselayerGroup.addLayer(newBaselayer);
+            }
+        }
+    }
+    toggleBaselayer(baselayerID, baselayer) {
+        var self = this;
+        if (typeof baselayer.url === "undefined") {
+        	console.error("Baselayer " + baselayerID + " must have a tile URL");
+        } else {
+            if (typeof this.baselayerIndex[baselayerID] === "undefined") {
+                this.baselayerIndex[baselayerID] = Object.assign({}, baselayer);
+                this.baselayerIndex[baselayerID].layer = L.tileLayer(baselayer.url);
+            }
+            if (baselayer.active === true) {
+                this.baselayerGroup.clearLayers();
+                this.baselayerGroup.addLayer(self.baselayerIndex[baselayerID].layer);
+            }
+        }
+    }
 }
--- src/js/components/LeafletMap.jsx
+++ src/js/components/LeafletMap.jsx
@@ -4,25 +4,45 @@ import ObliquePhotoMap from '../classes/ObliquePhotoMap.js';
 
 const mapStateToProps = function(store) {
   return {
-    layers: store.layers
+    layers: store.layers,
+    baselayers: store.baselayers
   };
 }
 
 class LeafletMap extends React.Component {
-	constructor () {
+	constructor (props) {
 		super();
 	}
 	componentDidMount () {
 		let map = this.refs.map;
 		this.map = new ObliquePhotoMap(map);
+		this.toggleLayers(null, this.props.layers);
+		this.toggleBaselayers(null, this.props.baselayers);
+	}
+	toggleLayers(oldLayerProps, newLayerProps) {
+		for(let layer in newLayerProps) {
+			if(oldLayerProps === null) {
+				this.map.toggleLayer(layer, newLayerProps[layer]);
+			}
+			else if(newLayerProps[layer].active !== oldLayerProps[layer].active) {
+				this.map.toggleLayer(layer, newLayerProps[layer]);
+			}
+		}
+	}
+	toggleBaselayers(oldBaselayerProps, newBaselayerProps) {
+		for(let baselayer in newBaselayerProps) {
+			if(oldBaselayerProps === null) {
+				this.map.toggleBaselayer(baselayer, newBaselayerProps[baselayer]);
+			}
+			else if(newBaselayerProps[baselayer].active !== oldBaselayerProps[baselayer].active) {
+				this.map.toggleBaselayer(baselayer, newBaselayerProps[baselayer]);
+			}
+		}
 	}
 	componentWillReceiveProps(nextProps) {
 		let oldProps = this.props;
-		for(let layer in nextProps.layers) {
-			if(nextProps.layers[layer].active !== oldProps.layers[layer].active) {
-				this.map.toggleLayer(layer, nextProps.layers[layer].active);
-			}
-		}
+		this.toggleLayers(oldProps.layers, nextProps.layers);
+		this.toggleBaselayers(oldProps.baselayers, nextProps.baselayers);
 	}
 	render () {
 		return (
