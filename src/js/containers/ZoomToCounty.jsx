6df7c58a14822fca75ecd75b5280bff443b2f9cc
CaitW <wolters.caitlin@gmail.com>
2017-02-18 15:12:12 -0500

    Fixed zoom to shoreline dropdown

--- src/js/containers/ZoomToCounty.jsx
+++ src/js/containers/ZoomToCounty.jsx
@@ -19,7 +19,7 @@ class ZoomToCounty extends React.Component {
     	var self = this;
     	var counties = [];
     	for(let countyName in CONFIG.map.county_shorelines) {
-    		counties.push(<MenuItem key={countyName} onClick={self.onMenuItemClick.bind(null, countyName)}>{countyName}</MenuItem>);
+    		counties.push(<MenuItem className="shoreline" key={countyName} onClick={self.onMenuItemClick.bind(null, countyName)}>{countyName.replace(/\_/g, " - ")}</MenuItem>);
     	}
     	return (
 			<NavDropdown title="Zoom To Shoreline" id="zoom-to-county">
