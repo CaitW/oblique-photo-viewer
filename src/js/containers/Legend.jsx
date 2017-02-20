4f8f1327b923b6e2d54c2c4d2319a5a1082812fb
CaitW <wolters.caitlin@gmail.com>
2017-02-20 17:56:28 -0500

    Add legend placeholder to sidebar

--- src/js/containers/Legend.jsx
+++ src/js/containers/Legend.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Panel, ListGroup } from 'react-bootstrap';
 
 export default class Legend extends React.Component {
 	constructor () {
 		super();
 	}
 	render () {
-
+		return (
+			<Panel header="Legend">
+				Legend
+			</Panel>
+		);
 	}
 }
--- src/js/containers/SideBar.jsx
+++ src/js/containers/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import LayerList from './LayerList.jsx';
+import Legend from './Legend.jsx';
 
 export default class SideBar extends React.Component {
 	constructor () {
@@ -10,6 +11,7 @@ export default class SideBar extends React.Component {
 		return (
 			<Col xsHidden sm={5} md={4} lg={3} className="sidebar">
 				<LayerList />
+				<Legend />
 			</Col>
 		)
 	}
