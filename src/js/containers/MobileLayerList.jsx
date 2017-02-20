ed245e339c3e719cedaf013ebdfb923909b83267
CaitW <wolters.caitlin@gmail.com>
2017-02-20 13:17:03 -0500

    refactor mobile layer list

--- src/js/containers/MobileLayerList.jsx
+++ src/js/containers/MobileLayerList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import LayerGroupList from '../components/LayerGroupList.jsx';
-import BasemapList from '../components/BasemapList.jsx';
+import LayerList from '../containers/LayerList.jsx';
 import { ListGroup, Accordion, Modal, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import store from '../store.js';
@@ -8,20 +7,12 @@ import { closeMobileLayerList } from '../actions.js';
 
 const mapStateToProps = function(store) {
     return {
-        layers: store.layers,
-        basemaps: store.basemaps,
         active: store.mobile.layersPopup.visible
     };
 }
 class MobileLayerList extends React.Component {
     constructor(props) {
         super(props);
-    }
-    onLayerClick(layerGroupID, layerID) {
-        store.dispatch(toggleLayer(layerGroupID, layerID));
-    }
-    onBasemapClick(basemapID) {
-        store.dispatch(toggleBasemap(basemapID));
     }
     closeModal () {
         store.dispatch(closeMobileLayerList());
@@ -38,8 +29,7 @@ class MobileLayerList extends React.Component {
                             <Modal.Title>Layers</Modal.Title>
                       </Modal.Header>
                       <Modal.Body>
-                        <BasemapList basemaps={this.props.basemaps} onBasemapClick={this.onBasemapClick}/>
-                        <LayerGroupList layers={this.props.layers} onLayerClick={this.onLayerClick}/>
+                        <LayerList />
                       </Modal.Body>
                       <Modal.Footer>
                             <Button onClick={this.closeModal}>Close</Button>
