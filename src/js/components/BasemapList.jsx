b357438d99a9cded7ace9a82fe5699393d2b5eb6
CaitW <wolters.caitlin@gmail.com>
2017-03-19 16:02:22 -0400

    prettify

--- src/js/components/BasemapList.jsx
+++ src/js/components/BasemapList.jsx
@@ -6,19 +6,18 @@ const BasemapList = (props) => {
     let basemaps = [];
     for (let basemapID in props.basemaps) {
         basemaps.push(
-            <Basemap 
-                key={basemapID} 
-                basemapID={basemapID} 
-                basemapName={props.basemaps[basemapID].basemapName}
-                active={props.basemaps[basemapID].active}
-                onBasemapClick={props.onBasemapClick}
+            <Basemap key={ basemapID }
+              basemapID={ basemapID }
+              basemapName={ props.basemaps[basemapID].basemapName }
+              active={ props.basemaps[basemapID].active }
+              onBasemapClick={ props.onBasemapClick }
             />
         );
     }
     let bodyClassNames = ["panel-body", "pullDown"];
     let headerClassNames = ["panel-heading"];
     let iconClassNames = ["fa"];
-    if(props.panelVisible === false) {
+    if (props.panelVisible === false) {
         bodyClassNames.push("hidden");
         iconClassNames.push("fa-plus");
     } else {
@@ -27,13 +26,15 @@ const BasemapList = (props) => {
     }
     return (
         <div className="panel panel-default">
-            <div className={headerClassNames.join(" ")} onClick={props.onPanelClick.bind(null, "Basemaps")}>Basemaps<i className={iconClassNames.join(" ")}></i></div>
-            <div className={bodyClassNames.join(" ")}>
-                <ListGroup>
-                    {basemaps}
-                </ListGroup>
-            </div>
+          <div className={ headerClassNames.join(" ") } onClick={ props.onPanelClick.bind(null, "Basemaps") }>
+            Basemaps<i className={ iconClassNames.join(" ") }></i>
+          </div>
+          <div className={ bodyClassNames.join(" ") }>
+            <ListGroup>
+              { basemaps }
+            </ListGroup>
+          </div>
         </div>
-    )
+    );
 };
 export default BasemapList;
