594d4f7a9f0add8ae6b388c69a86656a992ece0c
CaitW <wolters.caitlin@gmail.com>
2017-03-21 08:43:14 -0400

    Add extra preset for supporting ES7 functions

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015', 'react']
+                presets: ['es2015', 'react', 'stage-2'],
             }
         }, {
             test: /\.json$/,
