bb7a4a1a9231689a8f0cfe624bf0fdb4248a83c3
CaitW <wolters.caitlin@gmail.com>
2017-03-22 08:41:50 -0400

    Style modifications

--- src/js/containers/NavBar.jsx
+++ src/js/containers/NavBar.jsx
@@ -14,34 +14,42 @@ import { openMobileLayerList, openAboutModal } from '../actions.js';
 import store from '../store.js';
 
 export default class NavBar extends React.Component {
-	constructor () {
-		super();
-	}
-	onMobileLayersClick () {
-		store.dispatch(openMobileLayerList());
-	}
-	onAboutClick () {
-		store.dispatch(openAboutModal());
-	}
-	render () {
-		return (
-		  <Navbar collapseOnSelect>
-		    <Navbar.Header>
-		    <img src="img/wisconsin.svg" alt="Wisconsin Logo" className="hidden-xs"/>
-		      <Navbar.Brand>
-		        <a href="#">Wisconsin Shoreline Inventory and Oblique Photo Viewer</a>
-		      </Navbar.Brand>
-		      <Navbar.Toggle />
-		    </Navbar.Header>
-		    <Navbar.Collapse>
-		      <Nav>
-		        <NavItem eventKey={1} href="#" className='visible-xs-block' onClick={this.onMobileLayersClick}>Layers</NavItem>
-		        <ZoomToCounty />
-		      	<ResetView />
-		      	<NavItem eventKey={3} href="#" onClick={this.onAboutClick}>About</NavItem>
-		      </Nav>
-		    </Navbar.Collapse>
-		  </Navbar>
-		)
-	}
+    constructor() {
+        super();
+    }
+    onMobileLayersClick() {
+        store.dispatch(openMobileLayerList());
+    }
+    onAboutClick() {
+        store.dispatch(openAboutModal());
+    }
+    render() {
+        return (
+            <Navbar collapseOnSelect fluid>
+              <Navbar.Header>
+                <img src="img/wisconsin.svg" alt="Wisconsin Logo" className="hidden-xs" />
+                <Navbar.Brand>
+                  <a href="#">Wisconsin Shoreline Inventory and Oblique Viewer</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav pullRight>
+                  <NavItem eventKey={ 1 }
+                    href="#"
+                    className='visible-xs-block'
+                    onClick={ this.onMobileLayersClick }
+                  >
+                    Layers
+                  </NavItem>
+                  <ZoomToCounty />
+                  <ResetView />
+                  <NavItem eventKey={ 3 } href="#" onClick={ this.onAboutClick }>
+                    About
+                  </NavItem>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
