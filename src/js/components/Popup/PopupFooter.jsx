ee6f4e2b3ac92c98c8487ff389d752734c3df7d8
CaitW <wolters.caitlin@gmail.com>
2017-05-08 19:00:26 -0400

    separate out PopupFooter

--- src/js/components/Popup/PopupFooter.jsx
+++ src/js/components/Popup/PopupFooter.jsx
@@ -5,7 +5,7 @@ const PopupFooter = (props) => {
     let footer = [];
     switch (props.featureType) {
         case "photo": {
-            let photoURLs = getPhotoURLs(this.props.featureProperties);
+            let photoURLs = getPhotoURLs(props.featureProperties);
             footer.push(
                 <a href={photoURLs.original} key="open-larger-image-button" target="_blank" rel="noopener noreferrer" >
                     <Button className="open-larger-image-button">
@@ -16,20 +16,42 @@ const PopupFooter = (props) => {
             break;
         }
         case "profile": {
-          footer.push(
-            <a href="" key="open-larger-graph-button" target="_blank" rel="noopener noreferrer" >
-              <Button className="open-larger-graph-button">
-                <i className="fa fa-image"></i> Full-size Graph
-              </Button>
-            </a>
-          );
-          footer.push(
-            <a href="" key="download-excel-button" target="_blank" rel="noopener noreferrer" >
-              <Button className="download-excel-button">
-                <i className="fa fa-download"></i> Excel File
-              </Button>
-            </a>
-          );
+          if(props.featureProperties.bluff_xls !== false) {
+            footer.push(
+              <a href="" key="download-bluff-excel-button" target="_blank" rel="noopener noreferrer" >
+                <Button className="download-excel-button">
+                  <i className="fa fa-download"></i> Bluff Spreadsheet
+                </Button>
+              </a>
+            );
+          }
+          if(props.featureProperties.bathy_xls !== false) {
+            footer.push(
+              <a href="" key="download-bathy-excel-button" target="_blank" rel="noopener noreferrer" >
+                <Button className="download-excel-button">
+                  <i className="fa fa-download"></i> Bathy Spreadsheet
+                </Button>
+              </a>
+            );
+          }
+          if(props.featureProperties.bluff_jpg !== false) {
+            footer.push(
+              <a href="" key="open-larger-bluff-graph-button" target="_blank" rel="noopener noreferrer" >
+                <Button className="open-larger-graph-button">
+                  <i className="fa fa-image"></i> Full-size Bluff Graph
+                </Button>
+              </a>
+            );
+          }
+          if(props.featureProperties.bathy_png !== false) {
+            footer.push(
+              <a href="" key="open-larger-bathy-graph-button" target="_blank" rel="noopener noreferrer" >
+                <Button className="open-larger-graph-button">
+                  <i className="fa fa-image"></i> Full-size Bathy Graph
+                </Button>
+              </a>
+            );
+          }
           break;
         }
         default:
