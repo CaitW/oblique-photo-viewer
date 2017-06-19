46992221753b372adcfa6bbb129cb6f4cb4fdde2
CaitW <wolters.caitlin@gmail.com>
2017-06-19 09:40:02 -0500

    Added lake michigan records to popup, config

--- src/js/components/FeaturePopups/components/tabs/ProfileTab.jsx
+++ src/js/components/FeaturePopups/components/tabs/ProfileTab.jsx
@@ -23,7 +23,15 @@ class ProfileTab extends React.Component {
         var margin = { top: 10, right: 20, bottom: 30, left: 40 },
             width = 350 - margin.left - margin.right,
             height = 200 - margin.top - margin.bottom;
-        // Set the ranges
+        /**
+         * Scale Functions
+         * - Create the functions that accept a profile's data and scale each value
+         *  according to the size of the popup
+         *
+         * Y scale is normalized across all profiles and therefore the y axis
+         * remains consistent in popups, while x values are stretched according to each profile
+         * and will therefore vary depending on the profile being viewed
+         */
         var x = scaleLinear()
             .range([0, width]);
         var y = scaleLinear()
@@ -34,13 +42,38 @@ class ProfileTab extends React.Component {
             .ticks(5);
         var yAxis = axisLeft(y)
             .ticks(5);
-        // Define the line
+        /**
+         * Define the Lines
+         * - ValueLine is the actual profile line
+         * - michigan_avg, michigan_high, michigan_low are the lines that describe lake michigan avg lake levels
+         */
         var valueline = line()
             .x(function(d) {
                 return x(d.x);
             })
             .y(function(d) {
                 return y(d.y);
+            });
+        var michigan_avg = line()
+            .x(function(d) {
+                return (x(d.x))
+            })
+            .y(function (d) {
+                return y(CONFIG.lakeMichiganWaterLevel.avg)
+            });
+        var michigan_high = line()
+            .x(function(d) {
+                return (x(d.x))
+            })
+            .y(function (d) {
+                return y(CONFIG.lakeMichiganWaterLevel.recordHigh)
+            });
+        var michigan_low = line()
+            .x(function(d) {
+                return (x(d.x))
+            })
+            .y(function (d) {
+                return y(CONFIG.lakeMichiganWaterLevel.recordLow)
             });
         // Adds the svg canvas
         var svg = select(this.profileDiv)
@@ -49,11 +82,21 @@ class ProfileTab extends React.Component {
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // get the profile's data from it's json file
         json(this.props.jsonLocation, function(error, data) {
             data = data.sort(function (a, b) {
                 return ascending(a.x, b.x)
             });
             x.domain(extent(data, function(d) { return d.x }))
+            svg.append("path")
+                .attr("class", "lake-stats michigan-avg")
+                .attr("d", michigan_avg(data));
+            // svg.append("path")
+            //     .attr("class", "lake-stats michigan-low")
+            //     .attr("d", michigan_low(data));
+            // svg.append("path")
+            //     .attr("class", "lake-stats michigan-high")
+            //     .attr("d", michigan_high(data));
             // Add the valueline path.
             svg.append("path")
                 .attr("class", "line")
