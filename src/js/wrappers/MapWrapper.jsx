7dae3bc1d8b1ca7b20bf22bdcd80a2cfa13daaef
CaitW <wolters.caitlin@gmail.com>
2017-09-29 12:46:16 -0700

    Added roles to interactive buttons

--- src/js/wrappers/MapWrapper.jsx
+++ src/js/wrappers/MapWrapper.jsx
@@ -24,7 +24,7 @@ const MapWrapper = (props) => {
     }
     return (
         <Col xs={xs} sm={sm} md={md} lg={lg} className="wiscviewer-map-container">
-            <div className="wiscviewer-sidebar-toggle hidden-xs" onClick={props.toggleSidebar}>
+            <div className="wiscviewer-sidebar-toggle hidden-xs" onClick={props.toggleSidebar} role="button">
                 <i className={iconClassNames.join(' ')} />
             </div>
             {props.children}
